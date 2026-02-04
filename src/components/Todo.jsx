64e0f32aaf95b4689a29208830b8a92a321a22f3
Bulat <bulat067@gmail.com>
2026-02-04 07:16:06 +0300

    Button in the input box

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -10,8 +10,8 @@ const Todo = () => {
       </div>
 
       <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input type="text" placeholder='Добавьте свою задачу' />
-        <button>Добавить +</button>
+        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Добавьте свою задачу' />
+        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Добавить +</button>
       </div>
 
     </div>
