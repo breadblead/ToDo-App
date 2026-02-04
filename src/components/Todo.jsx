3e1cddae4e213bba96e962c57f3b0a43cd87333f
Bulat <bulat067@gmail.com>
2026-02-04 07:43:48 +0300

    Text input function

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import todo_icon from '../assets/todo_icon.png'
 import TodoItems from './TodoItems'
 
 const Todo = () => {
+
+const inputRef = useRef();
+
+const add = ()=> {
+ const inputText = inputRef.current.value.trim();
+ console.log(inputText);
+}
+
+
   return (
     <div className='bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl'>
       <div className='flex items-center mt-7 gap-2'>
@@ -12,8 +21,8 @@ const Todo = () => {
 
 
       <div className='flex items-center my-7 bg-gray-200 rounded-full'>
-        <input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Добавьте свою задачу' />
-        <button className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Добавить +</button>
+        <input ref={inputRef} className='bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600' type="text" placeholder='Добавьте свою задачу' />
+        <button onClick={add} className='border-none rounded-full bg-orange-600 w-32 h-14 text-white text-lg font-medium cursor-pointer'>Добавить +</button>
       </div>
 
       <div>
